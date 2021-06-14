442bcaafc8bddbe81ae315ee99026f05053ec1f3
kamilDawidowski <kamil.dawidowski@student.wat.edu.pl>
2021-06-14 18:16:55 +0200

    navigation and reservation

--- app/src/main/java/wat/mobilne/renthome/afterLogin/profile/ProfileFragment.kt
+++ app/src/main/java/wat/mobilne/renthome/afterLogin/profile/ProfileFragment.kt
@@ -40,25 +40,15 @@ class ProfileFragment : Fragment() {
             chEmail.visibility=View.VISIBLE
             chName.visibility=View.VISIBLE
             chSurname.visibility=View.VISIBLE
-            Toast.makeText(
-                context,
-                getString(R.string.InCorrectLogin),
-                Toast.LENGTH_SHORT
-            ).show()
+
+
         }
         btnChangePassword.setOnClickListener {
             val action =ProfileFragmentDirections.actionProfileFragmentToChangePasswordFragment()
             findNavController().navigate(action)
         }
 
-        btnGoToReservation.setOnClickListener {
 
-        }
-        floating_action_button_to_explore.setOnClickListener {
-//            val action =ProfileFragmentDirections.actionProfileFragmentToExploreFragment()
-//            findNavController().navigate(action)
-
-        }
 
 
         super.onViewCreated(view, savedInstanceState)
